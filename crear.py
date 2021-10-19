38f70f7b590ad5abbcf1bf3525aa673fb67e2aee
jeffmora <jeffersonmora92@gmail.com>
2021-10-19 00:36:23 -0500

    Interfaz finalizada: Agragada funcion para visualizar las imagenes por MshLab

--- crear.py
+++ crear.py
@@ -3,7 +3,6 @@
 # agregar las texturas al modelo computarizado.
 
 # Importamos librerias propias del sistama para trabajar. 
-
 import os
 import subprocess
 import sys
@@ -13,48 +12,119 @@ def run(*args):
     dir_coincidencias = args[1]
     par_sensor = args[2]
     dir_reconstruccion = args[3]
+    metodo_descriptor = args[4]
+    definicion = args[5]
+    modelo_geometrico = args[6]
+    metodo_coincidencia = args[7]
+    relacion_distancia = args[8]
+    refinamiento_intrinsecos = args[9]
+    metodo_triangulacion = args[10]
+    metodo_recesion = args[11]
+    avance = args[12]
 
-    #Analisís y tratamiendo de imagenes con OPENMVG para la reconstrucción stereo de multiples vistas.
-    print("\n 1. Anlisís de intrinsecos...")
-    pIntrisecos = subprocess.Popen(["openMVG_main_SfMInit_ImageListing", "-i", dir_entrada, "-o", dir_coincidencias, "-d", par_sensor])
-    pIntrisecos.wait()
-
-    print("\n 2. Calcular características...")
-    pCarater = subprocess.Popen(["openMVG_main_ComputeFeatures", "-i", dir_coincidencias+"/sfm_data.json", "-o", dir_coincidencias, "-m", "SIFT"])
-    pCarater.wait()
-
-    print("\n 3. Calcular coincidencias...")
-    pCoincid = subprocess.Popen(["openMVG_main_ComputeMatches", "-i", dir_coincidencias+"/sfm_data.json", "-o", dir_coincidencias])
-    pCoincid.wait()
-
-    print("\n 4. Reconstrucción incremental SfM...")
-    pRecons = subprocess.Popen(["openMVG_main_IncrementalSfM", "-i", dir_coincidencias+"/sfm_data.json", "-m", dir_coincidencias, "-o", dir_reconstruccion])
-    pRecons.wait()
-
-    print("\n 5. Colorear Estructura...")
-    pRecons = subprocess.Popen(["openMVG_main_ComputeSfM_DataColor", "-i", dir_reconstruccion+"/sfm_data.bin", "-o", os.path.join(dir_reconstruccion,"colorized.ply")])
-    pRecons.wait()
-    
-    # Convesión de MVG a MVS.
-    print("\n 6. Convesión de MVG a MVS...")
-    pConver = subprocess.Popen(["openMVG_main_openMVG2openMVS", "-i", dir_reconstruccion+"/sfm_data.bin", "-o", dir_reconstruccion+"/scene.mvs", "-d", dir_reconstruccion+"/scene_undistorted_images"])
-    pConver.wait()
-
-    # Proceso de reconstrucción final del modelo.
-    print("\n 7. Creación de nube de puntos densa...")
-    pDenPointCloud = subprocess.Popen(["/usr/local/bin/OpenMVS/DensifyPointCloud", dir_reconstruccion+"/scene.mvs"])
-    pDenPointCloud.wait()
-
-    print("\n 8. Reconstrucción de malla basada en nube de puntos, creación de modelo 3D...")
-    pRecMesh = subprocess.Popen(["/usr/local/bin/OpenMVS/ReconstructMesh", dir_reconstruccion+"/scene_dense.mvs"])
-    pRecMesh.wait()
-
-    print("\n 9. Reninamiento del modelo calculado...")
-    pRefMesh = subprocess.Popen(["usr/local/bin/OpenMVS/RefineMesh", dir_reconstruccion+"/scene_dense_mesh.mvs"])
-    pRefMesh.wait()
-
-    print("\n 10. Texturizado de modelo final...")
-    pTexMesh = subprocess.Popen(["/usr/local/bin/OpenMVS/TextureMesh", dir_reconstruccion+"/scene_dense_mesh_refine.mvs"])
-    pTexMesh.wait()
-
-    print("\n !Finalizado!")   
+    if avance == 1:
+        #Analisís y tratamiendo de imagenes con OPENMVG para la reconstrucción stereo de multiples vistas.
+        print("\n 1. Extraer parametros Exif de cámara...")
+        pIntrisecos = subprocess.Popen([
+            "openMVG_main_SfMInit_ImageListing", 
+            "-i", dir_entrada, 
+            "-o", dir_coincidencias, 
+            "-d", par_sensor, 
+            "-c", "2"])
+        pIntrisecos.wait()
+        return 2
+    elif avance == 2:
+        print("\n 2. Calcular características...")
+        pCarater = subprocess.Popen([
+            "openMVG_main_ComputeFeatures",
+            "-i", dir_coincidencias+"/sfm_data.json",
+            "-o", dir_coincidencias,
+            "-f", "1",
+            "-m", metodo_descriptor,
+            "-p", definicion,
+            "-n", "0"])
+        pCarater.wait()
+        return 3
+    elif avance == 3:
+        print("\n 3. Calcular coincidencias...")
+        pCoincid = subprocess.Popen([
+            "openMVG_main_ComputeMatches", 
+            "-i", dir_coincidencias+"/sfm_data.json", 
+            "-o", dir_coincidencias,
+            "-f", "1", 
+            "-r", relacion_distancia,
+            "-g", modelo_geometrico,
+            "-n", metodo_coincidencia])
+        pCoincid.wait()
+        return 4
+    elif avance == 4:
+        print("\n 4. Reconstrucción incremental SfM...")
+        pRecons = subprocess.Popen([
+            "openMVG_main_IncrementalSfM", 
+            "-i", dir_coincidencias+"/sfm_data.json", 
+            "-m", dir_coincidencias, 
+            "-o", dir_reconstruccion,
+            "-c", "2",
+            "-f", refinamiento_intrinsecos,
+            "-t", metodo_triangulacion,
+            "-r", metodo_recesion])
+        pRecons.wait()
+        return 5
+    elif avance == 5:
+        print("\n 5. Colorear Estructura...")
+        pRecons = subprocess.Popen([
+            "openMVG_main_ComputeSfM_DataColor", 
+            "-i", dir_reconstruccion+"/sfm_data.bin", 
+            "-o", os.path.join(dir_reconstruccion,"colorized.ply")])
+        pRecons.wait()
+        return 6
+    elif avance == 6:
+        # Convesión de MVG a MVS.
+        print("\n 6. Convesión de MVG a MVS...")
+        pConver = subprocess.Popen([
+            "openMVG_main_openMVG2openMVS", 
+            "-i", dir_reconstruccion+"/sfm_data.bin", 
+            "-o", dir_reconstruccion+"/scene.mvs", 
+            "-d", dir_reconstruccion+"/scene_undistorted_images",
+            "-n", "0"])
+        pConver.wait()
+        return 7
+    elif avance == 7:
+        # Proceso de reconstrucción final del modelo.
+        print("\n 7. Creación de nube de puntos densa...")
+        pDenPointCloud = subprocess.Popen([
+            "/usr/local/bin/OpenMVS/DensifyPointCloud", 
+            dir_reconstruccion+"/scene.mvs", 
+            "-w", dir_reconstruccion, 
+            "--max-threads", "0",
+            "--number-views", "0"])
+        pDenPointCloud.wait()
+        return 8
+    elif avance == 8:
+        print("\n 8. Reconstrucción de malla basada en nube de puntos, creación de modelo 3D...")
+        pRecMesh = subprocess.Popen([
+            "/usr/local/bin/OpenMVS/ReconstructMesh", 
+            dir_reconstruccion+"/scene_dense.mvs", 
+            "-w", dir_reconstruccion,
+            "--max-threads", "0"])
+        pRecMesh.wait()
+        return 9
+    elif avance == 9:
+        print("\n 9. Reninamiento del modelo calculado...")
+        pRefMesh = subprocess.Popen([
+            "/usr/local/bin/OpenMVS/RefineMesh", 
+            dir_reconstruccion+"/scene_dense_mesh.mvs", 
+            "-w", dir_reconstruccion,
+            "--max-threads", "0",
+            "--max-face-area", "32"])
+        pRefMesh.wait()
+        return 10
+    elif avance == 10:
+        print("\n 10. Texturizado de modelo final...")
+        pTexMesh = subprocess.Popen([
+            "/usr/local/bin/OpenMVS/TextureMesh", 
+            dir_reconstruccion+"/scene_dense_mesh_refine.mvs", 
+            "-w", dir_reconstruccion,
+            "--max-threads", "0"])
+        pTexMesh.wait()
+        return 11
--- main.py
+++ main.py
@@ -7,16 +7,18 @@
 
 
 # Importarmos todas las clases y metodos del la interfaz "app.py".
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from app import *
 from crear import *
-from PyQt5.QtWidgets import QFileDialog, QListWidget
+from PyQt5.QtWidgets import QFileDialog
 
 # Importamos librerias propias del sistema para trabajar.
 import os
 from os.path import expanduser
 import shutil
+import subprocess
 
 # Esta es la clase que crea la ventana de trabajo, tomando las carecteriticas de diseño y enlazando las
 # funciones de los botones de "app.py". 
@@ -26,11 +28,12 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-
+                
         # ============================== PARAMETROS INICIALES ============================== 
         self.stackedWidget.setCurrentWidget(self.pgn_inicio)
         self.BarraEstado.showMessage("Listo")
-        self.dir_imagenes = ""
+        self.configdefecto()
+        self.progressBar.setTextVisible(False)
 
         # ============================== RELACION BOTON-FUNCION ==============================
         # Relacionamos cada una de las señales que se emiten por la pulsacion de cada boton con una funcion
@@ -44,6 +47,10 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
         self.listaimg.itemClicked.connect(self.mostrarimagen)
         self.agregar.clicked.connect(self.importarimagenes)
         self.eliminar.clicked.connect(self.eliminarimagen)
+        self.Cargar.clicked.connect(self.cargarconfig)
+        self.ir_resultados.clicked.connect(self.resultados)
+        self.ir_importadas.clicked.connect(self.importadas)
+        self.Visualizar.clicked.connect(self.visualizar)
 
     # ============================== FUNCIONES ==============================
     
@@ -57,6 +64,7 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
             directory = expanduser("~")
         )
         if len(crear_directorio) != 0:
+            self.configdefecto()
             directorio = "imagenes"
             self.dir_imagenes = os.path.join(crear_directorio, directorio)
             if os.path.exists(self.dir_imagenes) == True:
@@ -64,6 +72,7 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
             else:
                 os.mkdir(self.dir_imagenes)
                 self.stackedWidget.setCurrentWidget(self.pgn_imagenes)
+                self.importadas()
                 self.BarraEstado.showMessage("Proyecto creado en '%s'." %crear_directorio, 2000)
                 self.ImportarImagenes.setEnabled(True)
                 self.leerdirectorio()
@@ -80,15 +89,20 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
             directory = expanduser("~")
         )
         if len(abrir_directorio) != 0:
+            self.configdefecto()
             directorio = "imagenes"
             self.dir_imagenes = os.path.join(abrir_directorio, directorio)
             if os.path.exists(self.dir_imagenes) == True:
                 self.BarraEstado.showMessage("Proyecto '%s' abierto correctamente." %abrir_directorio, 2000)
                 self.stackedWidget.setCurrentWidget(self.pgn_imagenes)
+                self.importadas()
                 self.ImportarImagenes.setEnabled(True)
                 self.leerdirectorio()
                 if self.leer_imagenes != []:
                     self.habilitarcrear3d()
+                    self.leerresultados()
+                    if self.leer_resultados != []:
+                        self.bloqueoinferfaz(ir_resultados="On")
             else:
                 self.BarraEstado.showMessage("El proyecto que intenta abrir no se ha creado, intente con 'Nuevo proyecto'.", 2000)
         else:
@@ -102,19 +116,33 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
     # Este metodo es el encargado de procesar cada imagen y generar el modelo 3D de la reconstrucción final.
     # Para esto se utiliza el script "crear.py" encargado de ejecutar cada una de la funciones de las librerias.
     def crearmodelo(self):
-        dir_entrada = self.dir_imagenes
+        self.dir_entrada = self.dir_imagenes
         dir_salida = self.dir_imagenes+"/../"
         dir_coincidencias = dir_salida+"coincidencias"
         dir_reconstruccion = dir_salida+"reconstruccion"
-        par_sensor = "/home/jefferson/Escritorio/tesis/openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt"
+        #par_sensor = "/home/jefferson/Escritorio/tesis/openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt"
 
         if not os.path.exists(dir_coincidencias):
             os.mkdir(dir_coincidencias)
 
         if not os.path.exists(dir_reconstruccion):
             os.mkdir(dir_reconstruccion)
-        
-        #run(dir_entrada, dir_coincidencias, par_sensor, dir_reconstruccion)
+        self.deshabilitarcrear3d()
+        self.bloqueoinferfaz(senal="On")
+        avance = 1
+        etapas = 10
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setMaximum(11)
+        self.progressBar.setValue(avance)
+        self.progressBar.setFormat("Trabajando...  %v/%m")
+        for e in range(etapas):
+            self.progressBar.setValue(run(self.dir_entrada, dir_coincidencias, self.par_sensor, dir_reconstruccion, self.sel1md, self.sel1ca, self.sel2mg,
+                self.sel2mc, self.reldist, self.sel3rf, self.sel3mt, self.sel3mr, avance))
+            avance += 1
+        self.progressBar.setFormat("Finalizado")
+        self.habilitarcrear3d()
+        self.stackedtrabajo.setCurrentWidget(self.pgn_resultados)
+        self.bloqueoinferfaz(senal="Off", ir_resultados="On")
     
     # ===== IMPORTAR IMAGENES =====
     # Permite al usuario importar cada una de las imagenes a procesar al directorio de trabajo creado o abierto
@@ -137,33 +165,46 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
         else:
             self.BarraEstado.showMessage("Seleccione las imagenes para continuar")
 
-    # ===== LEER DIRECTORIO =====
-    # Permite eliminar imagenes del directorio de trabajo.
+    # ===== ELIMINAR IMAGEN =====
+    # Permite eliminar imagenes del directorio de trabajo una vez se hayan importado.
     def eliminarimagen(self):
-        if self.selimagen:
-            os.remove(str(self.selimagen))
-            self.selimagen = []
-            self.leerdirectorio()
+        try:    
+            if self.selimagen:
+                os.remove(str(self.selimagen))
+                self.selimagen = []
+                self.leerdirectorio()
+        except AttributeError:
+            self.BarraEstado.showMessage("No hay elementos selecionados o la carpeta esta vacía.")
+        except FileNotFoundError:
+            self.BarraEstado.showMessage("El archivo no existe.")
 
     # ===== LEER DIRECTORIO =====
     # Comprobara la existencia de las imagenes en el directorio de trabajo y se presentaran en pantalla según su
     # disponibilidad.
     def leerdirectorio(self):
+        # Mostrar el nombre proyecto actual en el titulo de la ventana.
+        tituloproyecto = self.dir_imagenes.split('/')
+        self.setWindowTitle("%s - Reconstruccion 3D" %tituloproyecto[len(tituloproyecto)-2])
+        self.progressBar.setValue(0)
+        self.progressBar.setTextVisible(False)
         self.listaimg.clear()
         self.leer_imagenes = sorted(os.listdir(self.dir_imagenes))
         if self.leer_imagenes:
             self.listaimg.addItems(self.leer_imagenes)
-            self.visualimg.setPixmap(QPixmap(self.dir_imagenes+"/"+str(self.leer_imagenes[0])).scaledToWidth(400))
+            self.visualimg.setPixmap(QPixmap(self.dir_imagenes+"/"+str(self.leer_imagenes[0])).scaledToWidth(440))
         else:
             self.listaimg.addItem("Aún no hay imagenes importadas!")
-            self.visualimg.setPixmap(QPixmap("media/2130.png").scaledToWidth(400))
+            self.visualimg.setPixmap(QPixmap("media/2130.png").scaledToWidth(440))
     
     # ===== MOSTRAR IMAGEN =====
     # Como su nombre lo indica, permite visualizar escaladamente cada una de las imagenes que han sido importadas. El
     # usuario debera clickear sobre algun elemento de la lista para ver la imagen.
     def mostrarimagen(self):
-        self.selimagen = self.dir_imagenes+"/"+self.listaimg.currentItem().text()
-        self.visualimg.setPixmap(QPixmap(self.selimagen).scaledToWidth(400))
+        if self.listaimg.currentItem().text() == "Aún no hay imagenes importadas!":
+            self.visualimg.setPixmap(QPixmap("media/2130.png").scaledToWidth(440))
+        else:
+            self.selimagen = self.dir_imagenes+"/"+self.listaimg.currentItem().text()
+            self.visualimg.setPixmap(QPixmap(self.selimagen).scaledToWidth(440))
 
     # ===== HABILITAR CREAR 3D =====
     # Este metodo habilita el boton "crear modelo 3D" una vez se ha configurado el proyecto.  
@@ -173,9 +214,211 @@ class MainWindows(QtWidgets.QMainWindow, Ui_Aplicacion):
         "background-color: rgb(46, 52, 54);\n"
         "color: rgb(255, 255, 255);\n"
         "")
-   
+    
+    def deshabilitarcrear3d(self):
+        self.Crear3d.setEnabled(False)
+        self.Crear3d.setStyleSheet("border-radius: 0px;\n"
+        "background-color: rgb(136, 138, 133);\n"
+        "color: rgb(255, 255, 255);\n"
+        "")
+    
+    def bloqueoinferfaz(self, **kwords):
+        for k in kwords:
+            if k == "ir_resultados":
+                self.ir_resultados.setEnabled(True)
+                self.ir_resultados.setStyleSheet("border-radius: 0px;\n"
+                "background-color: rgb(46, 52, 54);\n"
+                "color: rgb(255, 255, 255);\n"
+                "")
+            elif k == "senal":
+                if kwords[k] == "On":
+                    self.NuevoProyecto.setEnabled(False)
+                    self.AbrirProyecto.setEnabled(False)
+                    self.ImportarImagenes.setEnabled(False)
+                    self.Salir.setEnabled(False)
+                    self.Cargar.setEnabled(False)
+                    self.Cargar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(136, 138, 133);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.agregar.setEnabled(False)
+                    self.agregar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(136, 138, 133);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.eliminar.setEnabled(False)
+                    self.eliminar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(136, 138, 133);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.ir_resultados.setEnabled(False)
+                    self.ir_resultados.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(136, 138, 133);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                else:
+                    self.NuevoProyecto.setEnabled(True)
+                    self.AbrirProyecto.setEnabled(True)
+                    self.ImportarImagenes.setEnabled(True)
+                    self.Salir.setEnabled(True)
+                    self.Cargar.setEnabled(True)
+                    self.Cargar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(46, 52, 54);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.agregar.setEnabled(True)
+                    self.agregar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(46, 52, 54);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.eliminar.setEnabled(True)
+                    self.eliminar.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(46, 52, 54);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
+                    self.ir_resultados.setEnabled(True)
+                    self.ir_resultados.setStyleSheet("border-radius: 0px;\n"
+                    "background-color: rgb(46, 52, 54);\n"
+                    "color: rgb(255, 255, 255);\n"
+                    "")
 
-# Bucle de trabajo.
+    # ===== CARGAR CONFIGURACION =====
+    # Este metodo permite cargar todas los paremetros definidos en cada una de las pestañas de la parte izquierda de
+    # la pantalla. Los parametros por defecto ya se encuentran cargados para ejecutar.
+    def cargarconfig(self):
+        
+        # ===== PARAMETROS DE CARACTERISTICAS =====
+        
+        # METODO DESCRIPTOR
+        if self.sel1mda.isChecked():
+            self.sel1md = "SIFT"
+        elif self.sel1mdb.isChecked():
+            self.sel1md = "AKAZE_FLOAT"
+        elif self.sel1mdc.isChecked():
+            self.sel1md = "AKAZE_MLDB"
+        
+        # DEFINICION
+        if self.sel1caa.isChecked():
+            self.sel1ca = "NORMAL"
+        elif self.sel1cab.isChecked():
+            self.sel1ca = "HIGH"
+        elif self.sel1cac.isChecked():
+            self.sel1ca = "ULTRA"
+        
+        # ===== PARAMETROS DE CORRESPONDENCIA =====
+
+        # MODELO GEOMETRICO
+        if self.sel2mga.isChecked():
+            self.sel2mg = "f"
+        elif self.sel2mgb.isChecked():
+            self.sel2mg = "e"
+        elif self.sel2mgc.isChecked():
+            self.sel2mg = "h"
+        elif self.sel2mgd.isChecked():
+            self.sel2mg = "a"
+        elif self.sel2mge.isChecked():
+            self.sel2mg = "o"
+        elif self.sel2mgf.isChecked():
+            self.sel2mg = "u"
+
+        # MODELO DE COINCIDENCIA
+        if self.sel2mca.isChecked():
+            self.sel2mc = "AUTO"
+        elif self.sel2mcb.isChecked():
+            self.sel2mc = "BRUTEFORCEL2"
+        elif self.sel2mcc.isChecked():
+            self.sel2mc = "ANNL2"
+        elif self.sel2mcd.isChecked():
+            self.sel2mc = "CASCADEHASHINGL2"
+        elif self.sel2mce.isChecked():
+            self.sel2mc = "FASTCASCADEHASHINGL2"
+        elif self.sel2mcf.isChecked():
+            self.sel2mc = "BRUTEFORCEHAMMING"
+        
+        # RELACION DE DISTANCIA
+        self.reldist = str(round(self.numrel.value(), 2))
+        
+        # ===== PARAMETROS DE SFM INCREMENTAL =====
+
+        # REFINAMIENTOS DE INTRINSECOS
+        if self.sel3rfa.isChecked():
+            self.sel3rf = "ADJUST_ALL"
+        elif self.sel3rfb.isChecked():
+            self.sel3rf = "NONE"
+        elif self.sel3rfc.isChecked():
+            self.sel3rf = "ADJUST_FOCAL_LENGTH"
+        elif self.sel3rfd.isChecked():
+            self.sel3rf = "ADJUST_PRINCIPAL_POINT"
+        elif self.sel3rfe.isChecked():
+            self.sel3rf = "ADJUST_DISTORTION"
+        
+        # METODO DE TRIANGULACION
+        if self.sel3mta.isChecked():
+            self.sel3mt = "0"
+        elif self.sel3mtb.isChecked():
+            self.sel3mt = "1"
+        elif self.sel3mtc.isChecked():
+            self.sel3mt = "2"
+        elif self.sel3mtd.isChecked():
+            self.sel3mt = "3"
+        
+        # METODO DE RECESION
+        if self.sel3mra.isChecked():
+            self.sel3mr = "0"
+        elif self.sel3mrb.isChecked():
+            self.sel3mr = "1"
+        elif self.sel3mrc.isChecked():
+            self.sel3mr = "2"
+        elif self.sel3mrd.isChecked():
+            self.sel3mr = "3"
+        elif self.sel3mre.isChecked():
+            self.sel3mr = "4"
+        self.BarraEstado.showMessage("Configuración cargada correctamente", 2000)
+
+    def configdefecto(self):
+        self.dir_imagenes = ""
+        self.sel1md = "SIFT"
+        self.sel1ca = "NORMAL"
+        self.sel2mg = "f"
+        self.sel2mc = "FASTCASCADEHASHINGL2"
+        self.reldist = "0.8"
+        self.sel3rf = "ADJUST_ALL"
+        self.sel3mt = "3"
+        self.sel3mr = "3"
+        usuariopc = expanduser("~")
+        data = "sensor_width_camera_database.txt"
+        buscar = subprocess.run(["find", usuariopc, "-name", data], capture_output=True)
+        buscar2 = os.path.split(buscar.stdout.decode())[0]
+        self.par_sensor = os.path.join(buscar2, data)
+        self.ir_resultados.setEnabled(False)
+
+    def leerresultados(self):
+        self.listares.clear()
+        self.dir_reconstruccion2 = os.path.join(self.dir_imagenes,"./../reconstruccion")
+        if os.path.exists(self.dir_reconstruccion2):
+            #self.leer_resultados = sorted(os.listdir(dir_reconstruccion))
+            #print(self.leer_resultados)
+            self.leer_resultados = sorted([file for file in os.listdir(self.dir_reconstruccion2) if '.ply' in file], reverse=True)
+            #print(self.leer_resultados2)
+            try:
+                self.leer_resultados
+                self.listares.addItems(self.leer_resultados)
+            except:
+                self.listaimg.addItem("Sin reconstrucciones generadas!")
+
+    def resultados(self):
+        self.stackedtrabajo.setCurrentWidget(self.pgn_resultados)
+
+    def importadas(self):
+        self.stackedtrabajo.setCurrentWidget(self.pgn_importadas)
+
+    def visualizar(self):
+        selresultado = self.dir_reconstruccion2+"/"+self.listares.currentItem().text()
+        pVis = subprocess.Popen(["meshlab", selresultado])
+        pVis.wait()
+
+        
+# ============================== BUCLE DE TRABAJO ==============================
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     windows = MainWindows()
